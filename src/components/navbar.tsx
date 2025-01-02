76482895471a40b89e16a7879cff807971f278a5
ToobaSaleem1234 <168776853+ToobaSaleem1234@users.noreply.github.com>
2025-01-03 01:14:57 +0500

    Add files via upload

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => (
 
   <header className="text-gray-600 body-font shadow-lg">
     <div className="container flex flex-wrap p-2 flex-col md:flex-row items-left sm:items-center">
-      <span className="flex-1 text-2xl text-black font-semibold sm:text-center">Toobas_Dynamic_Blog</span>
+      <span className="flex-1 text-2xl text-black font-semibold sm:text-center sm:pl-7">Toobas_Dynamic_Blog</span>
       <nav className="md:ml-auto flex flex-wrap  items-center justify-center text-gray-900 hover:underline cursor-pointer  ">
         <Link href={"/"} className="hover:text-blue-700 mr-10 transition-colors duration-300 transform hover:scale-105 ">Home</Link>
         <Link href={"/authorcard"} target="_blank" className="hover:text-blue-700 mr-10 transition-colors duration-300 transform hover:scale-105">About</Link>
@@ -15,7 +15,7 @@ const Navbar = () => (
         <Link href={"/contact"} target="_blank" className="hover:text-blue-700 mr-10 transition-colors duration-300 transform hover:scale-105">Contact</Link>
       </nav>
       <div>
-        <button className="w-36 px-6 items-center inline-flex bg-blue-700 border-0 py-3 focus:outline-none hover:bg-blue-500 rounded text-white mt-4 md:mt-0 lg:ml-32 lg:px-3 lg:py-1">
+        <button className="w-36 px-6 items-center inline-flex bg-blue-700 border-0 py-3 focus:outline-none hover:bg-blue-500 rounded text-white mt-4 md:mt-0 sm:pl-7 lg:ml-32 lg:px-3 lg:py-1">
           LinkedIn
           <svg
             fill="none"
