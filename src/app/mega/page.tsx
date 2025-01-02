eac8750dcd144c34a92821a6a3c5d9cde2a0c56a
ToobaSaleem1234 <168776853+ToobaSaleem1234@users.noreply.github.com>
2025-01-03 01:21:09 +0500

    Add files via upload

--- src/app/mega/page.tsx
+++ src/app/mega/page.tsx
@@ -92,7 +92,7 @@ const Mega = () => {
   return (
     <div className='my-8'>
       <h1 className='text-3xl font-bold text-center my-8 text-blue-700 animate-color-change '>Exploring the World Of Web Development </h1>
-      <div className='m-10 grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+      <div className='my-10 grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
         {post.map((post,index)=>(
           <div className='fade-in' key={post.id}>
             <div className='blog-card'>
